5294b043119dbf79aa31fb38d03d17d05322f994
chetanbundelkar <chetanchase1003@gmail.com>
2023-03-06 14:43:33 +0530

    gfg

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Quiz from "./Quiz";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Quiz />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+// OptionsQUestion
